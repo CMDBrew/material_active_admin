aa81c2c47b843b897ebac5a99090b891b872c628
Nelson Lee <ilung.me@gmail.com>
2017-04-20 11:38:10 -0700

    styling optimize & cleanup

--- lib/cb_activeadmin/extension/action_items.rb
+++ lib/cb_activeadmin/extension/action_items.rb
@@ -15,7 +15,7 @@ module ActiveAdmin
       def add_default_new_action_item
         add_action_item :new, only: :index do
           if controller.action_methods.include?('new') && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            link_to content_tag(:i, 'add_circle_outline', class: "cb_activeadmin-icons"), new_resource_path, :title => "#{I18n.t('active_admin.new_model', model: active_admin_config.resource_label)}", class: "nav-icon", :data => {toggle:"tooltip", placement:"bottom"}
+            link_to content_tag(:i, 'add_circle_outline', class: "cb_activeadmin-icons"), new_resource_path, :title => "#{I18n.t('active_admin.new_model', model: active_admin_config.resource_label)}", :data => {toggle:"tooltip", placement:"bottom"}
           end
         end
       end
@@ -24,7 +24,7 @@ module ActiveAdmin
       def add_default_edit_action_item
         add_action_item :edit, only: :show do
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            link_to content_tag(:i, 'mode_edit', class: "cb_activeadmin-icons"), edit_resource_path(resource), :title => "#{I18n.t('active_admin.edit_model', model: active_admin_config.resource_label)}", class: "nav-icon", :data => {toggle:"tooltip", placement:"bottom"}
+            link_to content_tag(:i, 'mode_edit', class: "cb_activeadmin-icons"), edit_resource_path(resource), :title => "#{I18n.t('active_admin.edit_model', model: active_admin_config.resource_label)}", :data => {toggle:"tooltip", placement:"bottom"}
           end
         end
       end
@@ -34,7 +34,7 @@ module ActiveAdmin
         add_action_item :destroy, only: :show do
           if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
             link_to content_tag(:i, 'delete', class: "cb_activeadmin-icons"), resource_path(resource),
-              method: :delete, :title => "#{I18n.t('active_admin.delete_model', model: active_admin_config.resource_label)}", class: "nav-icon", data: {confirm: I18n.t('active_admin.delete_confirmation'), toggle:"tooltip", placement:"bottom"}
+              method: :delete, :title => "#{I18n.t('active_admin.delete_model', model: active_admin_config.resource_label)}", data: {confirm: I18n.t('active_admin.delete_confirmation'), toggle:"tooltip", placement:"bottom"}
           end
         end
       end
