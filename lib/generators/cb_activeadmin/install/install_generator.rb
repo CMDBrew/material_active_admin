e8a5039c4bab75572b71fb39da22d2e22bcc6c75
Nelson Lee <nelson@cleverbanana.com>
2017-05-16 11:32:41 -0700

    remove active_admin:install && template replace to restrict for only theme support

--- lib/generators/cb_activeadmin/install/install_generator.rb
+++ lib/generators/cb_activeadmin/install/install_generator.rb
@@ -5,9 +5,9 @@ module CbActiveadmin
     desc "Installs CbActiveadmin and generates the necessary migrations & overwrites"
     source_root File.expand_path("../templates", __FILE__)
 
-    def install_active_admin
-      generate "active_admin:install"
-    end
+    # def install_active_admin
+    #   generate "active_admin:install"
+    # end
 
     def install_configs
       generate "cb_activeadmin:configs"
@@ -17,10 +17,10 @@ module CbActiveadmin
       generate "cb_activeadmin:assets"
     end
 
-    def replace_active_admin_defaults
-      template 'dashboard.rb.erb', 'app/admin/dashboard.rb'
-      template 'admin_user.rb.erb', 'app/admin/admin_user.rb'
-    end
+    # def replace_active_admin_defaults
+    #   template 'dashboard.rb.erb', 'app/admin/dashboard.rb'
+    #   template 'admin_user.rb.erb', 'app/admin/admin_user.rb'
+    # end
 
   end
 end
